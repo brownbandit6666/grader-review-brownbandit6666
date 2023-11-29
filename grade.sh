6abe7cbe69803a92591653babea2ae2d593209c0
Michael Brown <137442738+brownbandit6666@users.noreply.github.com>
2023-11-29 15:14:06 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,9 +8,40 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+expectedfile="student-submission/$2"
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+files=`find student-submission`
+FLAG=1
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+for file in $files
+do
+    if [[ -f $file && $file == $expectedfile ]]
+    then
+        echo "Correct file found"
+        FLAG=0
+        cp -r ./student-submission/*.java ./grading-area
+        cp -r ./*.java ./grading-area
+        cd ./grading-area
+        javac -cp $CPATH *.java
+        if [[ $? -ne 0 ]]
+        then
+            echo "compilation failed idiot"
+            exit
+        fi
+        java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > output.txt
+        grep -i -o "[0-9]*" output.txt > gr1.txt
+        grep "[0-9]*" gr1.txt | tail -2 > gr2.txt
+        TESTSRUN=`grep -m 1 "[0-9]*" gr2.txt`
+        TESTSFAIL=`grep "[0-9]*" gr2.txt | tail -1`
+        TESTSRUN=$(($TESTSRUN))
+        TESTSFAIL=$(($TESTSFAIL))
+        TESTSPASS=$(($TESTSRUN-$TESTSFAIL))
+        echo "Score: $(($TESTSPASS/$TESTSRUN))"
+    fi
+done
+
+if [[ $FLAG -eq 1 ]]
+then
+    echo "File not found"
+fi
+
